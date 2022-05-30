5d062e722ed533003a9887e59d8f3deb5af88b26
RevtiR <revatishah.rs@gmail.com>
2022-05-30 23:54:25 +0530

    Banner and Middle section have done

---
+++ convertAngular/src/app/middlecontent/middlecontent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-middlecontent',
+  templateUrl: './middlecontent.component.html',
+  styleUrls: ['./middlecontent.component.css']
+})
+export class MiddlecontentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
